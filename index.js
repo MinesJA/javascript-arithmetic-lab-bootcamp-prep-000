18b0e896fa009e13d39e4f44c4999ebc13a69e30
Jonathan Mines <minesja@gmail.com>
2017-09-16 00:04:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function add(a, b) {
 
 function subtract(a, b) {
   return a - b;
-}  
+}
 
 function multiply(a, b) {
   return a * b;
@@ -24,7 +24,7 @@ function dec(n) {
 
 function makeInt(n) {
   return parseInt(n, 10)
-}    
+}
 
 function preserveDecimal(n) {
   return parseFloat(n);
